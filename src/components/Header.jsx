e522f60346e1215282cecf9a2030c08ad83c040f
Donato Casiello <donato@MacBook-di-Donato.local>
2023-07-25 17:02:53 +0200

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <header>
+            <div className="left-side--header">
+                <img src="../src/assets/troll-face.png" alt="troll face" />
+                <h1>Meme Generator</h1>
+            </div>
+            <div className="right-side--header">React Course - Project 3</div>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Meme.jsx
@@ -0,0 +1,64 @@
+import React from "react"
+
+function Meme() {
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: ""
+    })
+
+    const [allMeme, setAllMeme] = React.useState([])
+
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+        .then(response => response.json())
+        .then(data => setAllMeme(data.data.memes))
+        
+    }, [])
+    console.log(allMeme)
+
+    function getNewMeme(){
+        const randomIndex = Math.floor(Math.random() * allMeme.length)
+        const url = allMeme[randomIndex].url
+        return url
+    }
+
+    function handleSetRandomMemeUrl(event) {
+        event.preventDefault()
+        const newUrl = getNewMeme()
+        setMeme(prevState => ({...prevState, randomImage: newUrl}))
+    }
+
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => (
+            {...prevMeme, 
+                [name]: value
+            }
+        ))
+    }
+
+
+
+
+    return (
+        <div >
+            <form className="form">
+                    <input type="text" placeholder="Input 1" name="topText" onChange={handleChange} />
+                    <input type="text" placeholder="Input 2" name="bottomText" onChange={handleChange} value={meme.bottomText}/>
+                <button 
+                    className="button--meme"
+                    onClick={handleSetRandomMemeUrl}>
+                    Get a new meme image
+                </button>
+            </form>
+            <div className="meme">
+                {meme.randomImage && <img className="meme--image" src={meme.randomImage}/>}
+                <h2 className="top-text---meme top">{meme.topText}</h2>
+                <h2 className="bottom-text---meme bottom">{meme.bottomText}</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Meme
