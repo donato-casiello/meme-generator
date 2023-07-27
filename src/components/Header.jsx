ca5547fb00efd0fad52a07f15e9db14f91a162ef
Donato Casiello <donato@MacBook-di-Donato.local>
2023-07-27 11:20:58 +0200

    Delete logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
     return (
         <header>
             <div className="left-side--header">
-                <img src="../src/assets/fun-white.png" alt="troll face" />
+                {/* <img src="../src/assets/fun-white.png" alt="troll face" /> */}
                 <h1>Meme Generator</h1>
             </div>
             <div className="right-side--header">React Course - Project 3</div>
